2ed9e6a7cd2dbe9d894dda01b2498ea21c56ee7b
Taenkun <taenkuntoy@gmail.com>
2026-09-22 02:54:11 +0700

    Add executable iszero.py for cell02 ex00

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+number_str = input("")
+number = int(number_str)
+
+if number == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
