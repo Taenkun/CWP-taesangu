32f60b4f6f4daa7891080aedcbb5e1d64a4874ae
Taenkun <taenkuntoy@gmail.com>
2026-09-22 16:58:13 +0700

    Add  play_with_arrays.py for cell05 ex03

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 old_array = [2, 8, 9, 48, 8, 22, -12, 2]
-new_array = [x + 2 for x in original_array if x > 5]
+new_array = [x + 2 for x in old_array if x > 5]
 
 print(old_array)
 # ทำให้เป็น set เพื่อตัดค่าที่ซ้ำกันออก
