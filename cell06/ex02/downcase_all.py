5501465aa7a563a9101e62e9a4ac03155c89446b
Taenkun <taenkuntoy@gmail.com>
2026-09-22 22:18:33 +0700

    Add downcase_all.py for cell06 ex02

---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+def downcase_it(text):
+    return text.lower()
+
+if len(sys.argv) < 2:
+    print("none")
+else:
+    for param in sys.argv[1:]:
+        print(downcase_it(param))
